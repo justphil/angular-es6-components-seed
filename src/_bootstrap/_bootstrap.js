09a615325f28e5bc79a7ce1282daa9eca5464776
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2016-04-20 19:00:47 +0200

    add basic folder structure for src/

---
+++ src/_bootstrap/_bootstrap.js
@@ -0,0 +1,29 @@
+/**
+ * 3rd-Party Libraries
+ */
+
+// Angular Core Libs (available via npm)
+import angular from 'angular';
+
+/**
+ * SPA code
+ */
+
+// Configs
+
+// Constants
+
+// Run Blocks
+
+// Controllers
+
+// Services
+
+// Filters
+
+// Directives
+
+/**
+ * Register all components within Angular.
+ */
+angular.module('myApp', []);
